f6aa5298f7de882b56507deb8e7060285a8396b7
Edgar <exgc12@gmail.com>
2017-06-26 11:30:11 -0400

    made it like it originally was

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.DATABASE_URL = "mongodb://Red:XAvier12@ds139342.mlab.com:39342/blog-challenge" || 
-                      process.env.DATABASE_URL ||
+exports.DATABASE_URL =
+                       process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        'mongodb://localhost/restaurants-app';
 exports.PORT = process.env.PORT || 8080;
